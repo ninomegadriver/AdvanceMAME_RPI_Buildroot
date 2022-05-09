f1e312348a5bee59654c8837ad7362d67f00caf8
nino@nino.com.br <nino@nino.com.br>
2022-05-09 20:40:36 -0300

    changed defaults to no interpolation

--- package/advancemame/advancemame-3.9/advance/osd/global.c
+++ package/advancemame/advancemame-3.9/advance/osd/global.c
@@ -1215,7 +1215,7 @@ adv_error advance_global_init(struct advance_global_context* context, adv_conf* 
 	unsigned i;
 
 	conf_int_register_enum_default(cfg_context, "config", conf_enum(OPTION_RESTORE), 1);
-	conf_bool_register_default(cfg_context, "misc_quiet", 0);
+	conf_bool_register_default(cfg_context, "misc_quiet", 1);
 	conf_int_register_enum_default(cfg_context, "misc_difficulty", conf_enum(OPTION_DIFFICULTY), DIFFICULTY_NONE);
 
 	OPTION_LANG[0].value = "none";
